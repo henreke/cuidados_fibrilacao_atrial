3304a81a3fa66860f14ba40e53529b47ffeb640a
henreke <henreke@hotmail.com>
2022-03-31 05:06:09 -0300

    Update jwtManager.dart

--- lib/utils/jwtManager.dart
+++ lib/utils/jwtManager.dart
@@ -1,39 +1,37 @@
 class jwtManager{
 
-const inMemoryJWTManager = () => {
-    let inMemoryJWT = null;
-    let jwtExpiricy = null;
-    let isRefreshing = null;
-    let logoutEventName = 'anjus-logout';
-    let refreshEndpoint = '/refresh-token';
-    let refreshTimeOutId;
-    let tipo_user = null;
+    late String inMemoryJWT;
+    late int jwtExpiricy ;
+    bool isRefreshing = false;
+    //let logoutEventName = 'anjus-logout';
+    //let refreshEndpoint = '/refresh-token';
+    //let refreshTimeOutId;
+    late int tipo_user = null;
 
-    const setLogoutEventName = name => logoutEventName = name;
-    const setRefreshTokenEndpoint = endpoint => refreshEndpoint = endpoint;
 
     // This countdown feature is used to renew the JWT before it's no longer valid
     // in a way that is transparent to the user.
     //com jwt 3
-    const refreshToken = (delay) => {
+    void refreshToken(delay){
         refreshTimeOutId = window.setTimeout(
             getRefreshedToken,
             delay * 1000 - 5000
         ); // Validity period of the token in seconds, minus 5 seconds
-    };
+    }
 
-    const abordRefreshToken = () => {
+    void abortRefreshToken(){
         if (refreshTimeOutId) {
             window.clearTimeout(refreshTimeOutId);
         }
-    };
+    }
 
-    const waitForTokenRefresh = () => {
+    bool waitForTokenRefresh(){
         
         if (!isRefreshing) {
         
             return Promise.resolve();
         }
+        await
         return isRefreshing.then(() => {
             isRefreshing = null;
             return true;
@@ -42,7 +40,7 @@ const inMemoryJWTManager = () => {
 
     // The method make a call to the refresh-token endpoint
     // If there is a valid cookie, the endpoint will set a fresh jwt in memory.
-    const getRefreshedToken = () => {
+    Future<bool> getRefreshedToken() async{
         const request = new Request(refreshEndpoint, {
             method: 'POST',
             headers: new Headers({ 'Content-Type': 'application/json' }),
@@ -73,56 +71,25 @@ const inMemoryJWTManager = () => {
             });
 
         return isRefreshing;
-    };
-    //original
-    const getRefreshedToken_origi = () => {
-        const request = new Request(refreshEndpoint, {
-            method: 'GET',
-            headers: new Headers({ 'Content-Type': 'application/json' }),
-            credentials: 'include',
-        });
-
-        isRefreshing = fetch(request)
-            .then((response) => {
-                if (response.status !== 200) {
-                    ereaseToken();
-                    global.console.log(
-                        'Token renewal failure'
-                    );
-                    return { token: null };
-                }
-                return response.json();
-            })
-            .then(({ token, tokenExpiry }) => {
-                if (token) {
-                    console.log('novo token');
-                    setToken(token, tokenExpiry);
-                    return true;
-                }
-                ereaseToken();
-                return false;
-            });
-
-        return isRefreshing;
-    };
+    }
 
 
-    const getToken = () => inMemoryJWT;
-    const getExpircy = () => jwtExpiricy;
-    const setToken = (token, delay) => {
+    String getToken() => return inMemoryJWT;
+    int getExpircy() => jwtExpiricy;
+  
+    bool setToken(token, delay){
         inMemoryJWT = token;
         jwtExpiricy = delay;
         refreshToken(delay);
         return true;
-    };
+    }
 
-    const ereaseToken = () => {
+    bool ereaseToken(){
         inMemoryJWT = null;
-        abordRefreshToken();
-        window.localStorage.setItem(logoutEventName, Date.now());
+        abortRefreshToken();
         return true;
     }
-    const getTipo_user = async (id) => {
+    Future<int> getTipo_user(id) async {
         
         if (tipo_user !== null){
             return tipo_user;
@@ -131,40 +98,15 @@ const inMemoryJWTManager = () => {
             tipo_user = await user.getTipoUser(id);
             return tipo_user;
         }
-    };
-    const setTipo_user = (tipo) =>{
+    }
+    setTipo_user(tipo){
         tipo_user = tipo;
         return true;
     }
 
-    const eraseTipo_user = () =>{
+    eraseTipo_user(){
         tipo_user = null;
         return true;
     }
-
-    // This listener will allow to disconnect a session of ra started in another tab
-    window.addEventListener('storage', (event) => {
-        if (event.key === logoutEventName) {
-            inMemoryJWT = null;
-            tipo_user = null;
-        }
-    });
-
-    return {
-        ereaseToken,
-        getRefreshedToken,
-        getToken,
-        setLogoutEventName,
-        setRefreshTokenEndpoint,
-        setToken,
-        waitForTokenRefresh,
-        setTipo_user,
-        eraseTipo_user,
-        getTipo_user,
-        getExpircy
-
-
-    }
-};
 
 }
